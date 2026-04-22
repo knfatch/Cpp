8ce81b0838f54701fd7793eddb21b8f849a05b15
knfatch <knfatch@gmail.com>
2026-04-22 18:34:04 -0400

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,7 @@
+// First C++ program: Outputs "Hello, World!" to the console
+#include <iostream>                             // Include the iostream library for input and output operations
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;  //Prints text to screen
+    return 0;                                   // Return 0 indicates that the program ended successfully
+}
